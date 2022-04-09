feb0b6466db6436ea331ed3cf901a15131934c28
s7uck <stuck@stuckduck.it>
2022-04-09 15:29:55 +0200

    Cleaned up the assets index

--- res/dynamic.js
+++ res/dynamic.js
@@ -1,7 +1,7 @@
 /*
 	Appends dynamic properties to the resources object
 */
-res.pages['_home'] = $('main > section').attr('id') ?? "#_null";
-res.themes['_auto'] = window.matchMedia('(prefers-color-scheme: light)').matches
-										? res.themes.light
-										: res.themes.dark;
+pages['_home'] = $('main > section').attr('id') ?? "#_null";
+themes['_auto'] = window.matchMedia('(prefers-color-scheme: light)').matches
+	                ? themes.light
+                  : themes.dark;
--- res/index.js
+++ res/index.js
@@ -1,39 +1,32 @@
 /*
 	Webpage resources: pages, styles, content, scripts etc.
 */
-const res = {
-	"html": {
-		"_index": "index.html",
-		"index": "index.html",
-		"navbar": "#navbar"
-	},
-	"pages": {
-		"home": "#_home",
-		"debugging": "#debugging",
-		"dashboard": "#dashboard",
-		"settings": "#settings"
-	},
-	"css": {
-		"colorScheme": "#colorscheme",
-		"style": "./src/css/style.css",
-		"static": "./src/css/static.css",
-	},
-	"ui": {
-		"mobile": [],
-		"tablet": [],
-		"desktop": []
-	},
-	"themes": {
-		"dark": "./src/css/theme/dark.css",
-		"light": "./src/css/theme/light.css"
-	},
-	"js": {
-		"utils": "./src/js/utils.js",
-		"main": "./src/js/index.js",
-		"debugger": "./src/js/debugger.js"
-	},
-	"data": {
-		"settings": "./content/byUser/settings.json",
-		"defaults": "./content/byUser/defaults.json"
-	}
+let markup = {
+	_index: "index.html",
+	index: this._index
+};
+let pages = {
+	_home: '#_home',
+	home: this.newLink,
+	newLink: '#newLink',
+	debugging: '#debugging',
+	dashboard: '#dashboard',
+	settings: '#settings'
+};
+let stylesheets = {
+	style: "./src/css/style.css",
+	static: "./src/css/static.css"
+};
+let themes = {
+	dark: "./src/css/theme/dark.css",
+	light: "./src/css/theme/light.css"
+};
+let scripts = {
+	util: "./src/js/utils.js",
+	main: "./src/js/index.js",
+	debug: "./src/js/debugger.js"
+};
+let data = {
+	userSettings: "./content/byUser/settings.js",
+	defaults: "./content/byUser/defaults.js"
 };
