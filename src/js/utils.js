f51b83ca27e834420c8b03180421d775194dc9af
s7uck <stuck@stuckduck.it>
2022-04-10 08:23:37 +0200

    Made the URL validation easier on users

--- src/js/utils.js
+++ src/js/utils.js
@@ -17,6 +17,47 @@ function refresh() {
 	go('');
 }
 
+/* Validate URL */
+function isValidURL(string) {
+	let url;
+	try {
+		url = new URL(string);
+	} catch (e) {
+		return false;
+	}
+
+	return (url.protocol === 'http:'
+		|| url.protocol === 'https:')
+		&& url.hostname.includes('.');
+}
+
+/* Move to a specific section of the website */
+function moveTo(page) {
+	let path = page;
+	// Check if the page exists in the pages list
+	// If so, move to the associated path
+
+	if ( page in pages ) { path = pages[page]; }
+	// Add a leading # hash to the given string if not already present
+	if ( !path.startsWith("#") ) { path = `#${path}`; }
+
+	// Move to the specified section; it just adds #section to the URL
+	window.location.hash = path;
+}
+
+/* Get or set the value a CSS style variable */
+function css(name, value = null) {
+	// Check if the name starts with -- (the CSS property prefix)
+	// if it doesn't, prepend it automatically
+	if ( !name.startsWith('--') ) { name = `--${name}`; }
+
+	// If specified, change the variable's value
+	if ( value !== null )
+		return $(':root').css(name, value);
+	// Else return the value using jQuery
+	return $(':root').css(name);
+}
+
 /* Handling screen sizes */
 let screenSmallerThan =(size) => {
 	return window.matchMedia(
@@ -27,30 +68,4 @@ let screenLargerThan =(size) => {
 	return window.matchMedia(
 		`only screen and (min-width: ${size}px)`
 	).matches;
-}
-
-/* Move to a specific section of the website */
-function moveTo(page) {
-	let path = page;
-	// Check if the page exists in the pages list
-	// If so, move to the associated path
-
-	if (page in pages) { path = pages[page]; }
-	// Add a leading # hash to the given string if not already present
-	if (!path.startsWith("#")) { path = `#${path}`; }
-
-	// Move to the specified section; it just adds #section to the URL
-	window.location.hash = path;
-}
-
-/* Get or set the value a CSS style variable */
-function css(name, value = null) {
-	// Check if the name starts with -- (the CSS property prefix)
-	// if it doesn't, prepend it automatically
-	if (!name.startsWith('--')) { name = `--${name}`; }
-
-	// If specified, change the variable's value
-	if (value !== null) return $(':root').css(name, value);
-	// Else return the value using jQuery
-	return $(':root').css(name);
 }
