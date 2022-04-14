866330f1569ed2b4ef200878e36a3bbdd64a8456
hd917 <stuck@stuckduck.it>
2022-04-14 17:03:42 +0200

    Removed the Debug page

--- src/js/index.js
+++ src/js/index.js
@@ -36,24 +36,3 @@ $(urlbox).on('invalid', (event) => {
 		form.submit();
 	}
 });
-
-/* JS debug page code */
-$(debug).on('input', (event) => {
-	event.preventDefault();
-	debug(
-		$(repl).val(), (result, error)=> {
-			let infoType = error ? 'error' : 'recommended';
-			let infoTitle = error ? error.constructor.name : $(repl).val();
-			let content = error
-				? error.message.replace(`${error.constructor.name}: `, '')
-				: result;
-			let replColor = error ? 'var(--desert-red-8)' : 'var(--accent)';
-			$(repl).css('color', replColor);
-			$('#result')
-			.attr('class', infoType)
-			.attr('title', infoTitle)
-			.text(content);
-		}
-	);
-	if ( $(repl).val() === "2+3" ) { go("https://billwurtz.com/2plus3.mp4"); }
-});
